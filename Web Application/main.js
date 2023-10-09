8fd60506614957763ab089f2d5a85338e98e6361
shaunahurley <hurley65@students.rowan.edu>
2023-10-09 01:02:46 -0400

    View Button Functionality
    Added an id to the button so I could reference it in the JS. Added a dummy html file that the database results would show up in. Added the js code so when you click the view button you move into the dummy file.

--- Web Application/main.js
+++ Web Application/main.js
@@ -1,1 +1,8 @@
+window.addEventListener("load", function() {
+    const viewBut = document.getElementById("view");
+    viewBut.addEventListener("click", action);
+})
 
+function action() {
+    window.location.href = "database.html";
+}

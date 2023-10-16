e15dce702ca8a4b32647e5f24819655fca99a780
Michael Loughrin <loughr95@students.rowan.edu>
2023-10-16 02:01:30 -0400

    baseline to db modification

--- app.py
+++ app.py
@@ -10,8 +10,36 @@ app.config['MYSQL_DB'] = 'essiga27'
 
 db = MySQL(app)
 
-@app.route('/allocate', methods=['POST'])
-def process_form():
+
+@app.route('/allocate/f', methods=['POST'])
+def allocate_f():
+    user = request.form.get('user')
+    network = request.form.get('network')
+    
+    cur = db.connection.cursor()    
+    # update db
+       
+       
+    cur.close()
+    
+    return user + " assigned to " + network
+
+@app.route('/allocate')
+def select_page():
+    cur = db.connection.cursor()
+    
+    cur.execute("select username from user")
+    column1_values = cur.fetchall()
+    
+    cur.execute("select name from network")
+    column2_values = cur.fetchall()
+    
+    cur.close()
+    return render_template('select.html', column1_values=column1_values, column2_values=column2_values)
+
+
+@app.route('/login', methods=['POST'])
+def login():
     # can now use in backend like 
     # validating
     user = request.form.get('username')
@@ -22,7 +50,7 @@ def process_form():
 @app.route('/avail')
 def print_resources():
     try:
-        table_to_query = "user"
+        table_to_query = "network"
         cursor = db.connection.cursor()
 
         cursor.execute("select * from " + table_to_query)
@@ -30,7 +58,6 @@ def print_resources():
 
         cursor.execute("show columns from " + table_to_query)
         cols = cursor.fetchall()
-        
         cursor.close()
         
         # render the new template (html file)
